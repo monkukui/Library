d339a9324944796a3cdded27131ea77ef379c834
monkukui <monkukui1953@gmail.com>
2019-06-23 12:44:57 +0900

    Dijkstraの修正

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,57 +1,32 @@
-template<typename Weight>
-struct edge{
-    size_t src, dst;
-    Weight cost;
+// verified : http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
 
-    edge() = default;
-    edge(size_t src, size_t dst, Weight const& cost): src(src), dst(dst), cost(cost){}
+template<typename T>
+vector<T> dijkstra(vector<vector<pair<int, T> &g, int s){
+    
+    int n = g.size();
+    // numeric_limits がうまく使えない場合は自分で INF を定義する.
+    const auto INF = numeric_limits<T>::max();
+    vector<T> dist(n, INF);
 
-    bool operator <(edge const &other) const{
-        if(cost != other.cost) return cost < other.cost;
-        if(src != other.src) return src > other.src;
-        return dst < other.dst;
-    }
-};
-
-template<typename Weight>
-struct graph: std::vector<std::vector<edge<Weight>>>{
-    graph(size_t n): std::vector<std::vector<edge<Weight>>>(n){}
-
-    void connect_to(size_t src, size_t dst, Weight const& cost){
-        (*this)[src].emplace_back(src, dst, cost);
-    }
-
-    void connect_with(size_t src, size_t dst, Weight const& cost){
-        connect_to(src, dst, cost);
-        connect_to(dst, src, cost);
-    }
-};
-
-template<typename Weight>
-std::vector<Weight> shortest(graph<Weight> const &g, size_t s = 0, Weight inf = numeric_limits<Weight>::max(), Weight ei = Weight()){
-    // based on Dijkstra algorithm
-    size_t n = g.size();
-    std::vector<Weight> res(n, inf);
-    using P = std::pair<Weight, size_t>;
-    std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
-    res[s] = ei;
-    pq.emplace(res[s], s);
-
-    while(!pq.empty()){
-        Weight cur_cost;
-        size_t cur_node;
-        std::tie(cur_cost, cur_node) = pq.top();
-        pq.pop();
-        if(res[cur_node] < cur_cost) continue;
-        for(auto const& e: g[cur_node]){
-            Weight next_cost = cur_cost+ e.cost;
-            size_t next_node = e.dst;
-            if(res[next_node] > next_cost){
-                res[next_node] = next_cost;
-                pq.emplace(next_cost, next_node);
-            }
+    using P = pair<T, int>;
+    // 小さい順に取り出せる
+    priority<queue<P, vector<P>, greater<P>> que;
+    dist[s] = 0;
+    que.emplace(dist[s], s);
+    while(!que.empty()){
+        T cost;
+        int node;
+        tie(cost, node) = que.top();
+        que.pop();
+        if(dist[node] < cost) continue;
+        for(auto &e : g[node]){
+            auto next_cost = cost + e.second;
+            int next_node = e.first;
+            if(dist[next_node] <= next_cost) continue;
+            dist[next_node] = next_cost;
+            que.emplace(next_cost, next_node);
         }
     }
 
-    return res;
+    return dist;
 }
