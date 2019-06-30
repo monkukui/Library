b97dc7d3c6a83c417f41d70d070a33a6d4095637
monkukui <monkukui1953@gmail.com>
2019-06-30 11:41:57 +0900

    Math の修正

--- Math/base.cpp
+++ Math/base.cpp
@@ -27,6 +27,19 @@ int prime[MAXN];        // [a,b) の素数のうち i 番目の素数
 bool is_prime[MAXN];    // 整数 i が素数であるかどうか
 bool is_prime_ab[MAXN]; // 整数 i+a が素数であるかどうか
 
+int sieve(int n) {
+    int res = 0;
+    fill(is_prime, is_prime + MAXN, true);
+    is_prime[0] = is_prime[1] = false; // 0 と 1 は素数ではない。
+    for(int i = 2; i <= n; ++i) {
+        if(!is_prime[i]) continue;
+        prime[res++] = i;
+        for(int j = 2 * i; j <= n; j += i) is_prime[j] = false; // 素数 i の倍数は素数ではない (ふるい(篩)にかける)
+    }
+    return res;
+}
+
+/*
 int segment_sieve(int a, int b){
     fill(is_prime, is_prime + MAXN, true);
     fill(is_prime_ab, is_prime_ab + MAXN, true);
@@ -43,6 +56,7 @@ int segment_sieve(int a, int b){
     for(int i = a; i < b; i++) if(is_prime_ab[i - a]) prime[res++] = i;
     return res;
 }
+*/
 
 // ある整数の約数列挙
 vector<int> divisors(int n) {
