260f4aa4b69ea4c5fa906fbc1f50e87b55c9e52c
monkukui <monkukui1953@gmail.com>
2019-08-20 14:50:40 +0900

    抽象累積和ライブラリの追加

---
+++ DataStructure/CumSum.cpp
@@ -0,0 +1,26 @@
+// 抽象累積和
+// 構築 O(n), get O(1)
+
+template<typename T>
+struct CumSum{
+private: 
+  size_t n;
+  vector<T> dat;
+
+public: 
+  CumSum(vector<T> v){
+    n = v.size();
+    dat.resize(n + 1, 0);
+    for(size_t i = 0; i < n; i++){
+      dat[i + 1] = dat[i] + v[i];
+    }
+  }
+
+  T get(size_t r){ // 0-indexed, [0. r)
+    return dat[r];
+  }
+
+  T get(size_t l, size_t r){ // 0-indexed, [l, r)
+    return dat[r] - dat[l];
+  }
+};
