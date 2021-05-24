337aff59c611e5ec0bb637cc4c13b71aa70462e4
monkukui <monkukui1953@gmail.com>
2021-05-24 15:26:33 +0900

    add combtable

---
+++ Math/CombTable.cpp
@@ -0,0 +1,20 @@
+// nCr を計算する
+// 前処理 O(n ^ 2)
+// クエリ O(1)
+struct CombModP {
+    vector<vector<long long>> com;
+    void init(long long n) {
+        com.assign(n, vector<long long>(n));
+        com[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+              if (j + 1 < n) com[i + 1][j + 1] += com[i][j];
+              com[i + 1][j] += com[i][j];
+            }
+        }
+    }
+    long long nCk(long long n, long long k) {
+        return com[n][k];
+    }
+};
+
