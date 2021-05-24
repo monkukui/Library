41770d51b7b82d39535ac97996d5c2138739f53d
monkukui <monkukui1953@gmail.com>
2021-05-24 15:29:41 +0900

    add combtable

--- Math/CombTable.cpp
+++ Math/CombTable.cpp
@@ -4,11 +4,11 @@
 struct CombModP {
     vector<vector<long long>> com;
     void init(long long n) {
-        com.assign(n, vector<long long>(n));
+        com.assign(n + 1, vector<long long>(n + 1, 0));
         com[0][0] = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= n; j++) {
-              if (j + 1 < n) com[i + 1][j + 1] += com[i][j];
+              if (j + 1 <= n) com[i + 1][j + 1] += com[i][j];
               com[i + 1][j] += com[i][j];
             }
         }
